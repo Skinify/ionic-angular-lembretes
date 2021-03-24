05d395f9e6bf42d518f9306147459d15d56fd72b
Skinify <gabrielZK2000@hotmail.com>
2021-03-24 12:27:39 -0300

    Status bar customizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  providers: [StatusBar]
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private statusBar: StatusBar) {
+    this.statusBar.backgroundColorByHexString('#f9fafb');
+    this.statusBar.styleDefault()
+    this.statusBar.overlaysWebView(true);
+  }
 }
